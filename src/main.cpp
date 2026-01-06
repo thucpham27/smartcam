75e70d66e6ede0277ac8793127a7a26a50d9f132
thuc <thuc5a2@gmail.com>
2026-01-06 13:47:13 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -535,7 +535,7 @@ main (int argc, char *argv[])
 
         if (!nodet) {
             sprintf(pip + strlen(pip), " ! tee name=t \
-                    ! queue ! vvas_xmultisrc kconfig=\"%s/preprocess.json\" \
+                    ! queue ! vvas_xabrscaler kconfig=\"%s/preprocess.json\" \
                     ! queue ! vvas_xfilter kernels-config=\"%s/aiinference.json\" \
                     ! ima.sink_master \
                     vvas_xmetaaffixer name=ima ima.src_master ! fakesink \
